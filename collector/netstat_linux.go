86ec1a2e9d638f8379d1a74de9fb1494c03cfaae
david serck <david.serck@destiny.eu>
2021-02-03 14:30:14 +0100

    monitor ephemeral ports with netstat collector

--- collector/netstat_linux.go
+++ collector/netstat_linux.go
@@ -17,6 +17,7 @@ package collector
 
 import (
 	"bufio"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -35,12 +36,17 @@ const (
 )
 
 var (
-	netStatFields = kingpin.Flag("collector.netstat.fields", "Regexp of fields to return for netstat collector.").Default("^(.*_(InErrors|InErrs)|Ip_Forwarding|Ip(6|Ext)_(InOctets|OutOctets)|Icmp6?_(InMsgs|OutMsgs)|TcpExt_(Listen.*|Syncookies.*|TCPSynRetrans)|Tcp_(ActiveOpens|InSegs|OutSegs|OutRsts|PassiveOpens|RetransSegs|CurrEstab)|Udp6?_(InDatagrams|OutDatagrams|NoPorts|RcvbufErrors|SndbufErrors))$").String()
+	netStatFields = kingpin.Flag("collector.netstat.fields", "Regexp of fields to return for netstat collector.").Default("^(.*_(InErrors|InErrs)|Ip_Forwarding|Ip(6|Ext)_(InOctets|OutOctets)|Icmp6?_(InMsgs|OutMsgs)|TcpExt_(Listen.*|Syncookies.*|TCPSynRetrans)|Tcp_(ActiveOpens|InSegs|OutSegs|OutRsts|PassiveOpens|RetransSegs|CurrEstab)|Udp6?_(InDatagrams|OutDatagrams|NoPorts|RcvbufErrors|SndbufErrors)|IpLocalPortRange?_(min|max)|TcpSocket?_(NbUsedPort)|UdpSocket?_(NbUsedPort))$").String()
 )
 
 type netStatCollector struct {
 	fieldPattern *regexp.Regexp
 	logger       log.Logger
+}
+type ephemeralPortTuple struct {
+	localIP    string
+	remoteIP   string
+	remotePort string
 }
 
 func init() {
@@ -70,6 +76,19 @@ func (c *netStatCollector) Update(ch chan<- prometheus.Metric) error {
 	if err != nil {
 		return fmt.Errorf("couldn't get SNMP6 stats: %w", err)
 	}
+	ipLocalPortRangeStats, err := getIPLocalPortRange(procFilePath("sys/net/ipv4/ip_local_port_range"))
+	if err != nil {
+		return fmt.Errorf("couldn't get ip local port range stats: %w", err)
+	}
+	tcpSocketsStats, err := getSocketsStats(procFilePath("net/tcp"))
+	if err != nil {
+		return fmt.Errorf("couldn't get tcp socket stats: %w", err)
+	}
+	udpSocketsStats, err := getSocketsStats(procFilePath("net/udp"))
+	if err != nil {
+		return fmt.Errorf("couldn't get udp socket stats: %w", err)
+	}
+
 	// Merge the results of snmpStats into netStats (collisions are possible, but
 	// we know that the keys are always unique for the given use case).
 	for k, v := range snmpStats {
@@ -78,11 +97,15 @@ func (c *netStatCollector) Update(ch chan<- prometheus.Metric) error {
 	for k, v := range snmp6Stats {
 		netStats[k] = v
 	}
+	for k, v := range ipLocalPortRangeStats {
+		netStats[k] = v
+	}
 	for protocol, protocolStats := range netStats {
 		for name, value := range protocolStats {
 			key := protocol + "_" + name
 			v, err := strconv.ParseFloat(value, 64)
 			if err != nil {
+				fmt.Printf("invalid value %s in netstats", value)
 				return fmt.Errorf("invalid value %s in netstats: %w", value, err)
 			}
 			if !c.fieldPattern.MatchString(key) {
@@ -98,6 +121,33 @@ func (c *netStatCollector) Update(ch chan<- prometheus.Metric) error {
 			)
 		}
 	}
+
+	// Merge the results of udpSocketsStats into tcpSocketsStats
+	for k, v := range udpSocketsStats {
+		tcpSocketsStats[k] = v
+	}
+	for protocol, protocolStats := range tcpSocketsStats {
+		for name, v := range protocolStats {
+			key := protocol + "_" + name
+			if !c.fieldPattern.MatchString(key) {
+				continue
+			}
+			for tupleForEphemeralPort, value := range v {
+				ch <- prometheus.MustNewConstMetric(
+					prometheus.NewDesc(
+						prometheus.BuildFQName(namespace, netStatsSubsystem, key),
+						fmt.Sprintf("Statistic %s.", protocol+name),
+						[]string{"localIp", "remoteIp", "remotePort"},
+						nil,
+					),
+					prometheus.CounterValue,
+					float64(value),
+					tupleForEphemeralPort.localIP, tupleForEphemeralPort.remoteIP, tupleForEphemeralPort.remotePort,
+				)
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -175,4 +225,111 @@ func parseSNMP6Stats(r io.Reader) (map[string]map[string]string, error) {
 	}
 
 	return netStats, scanner.Err()
+}
+
+//get ephemeral ports range
+func getIPLocalPortRange(fileName string) (map[string]map[string]string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+	defer file.Close()
+
+	return parseIPLocalPortRange(file)
+}
+
+func parseIPLocalPortRange(r io.Reader) (map[string]map[string]string, error) {
+	var (
+		ipLocalPortRange = map[string]map[string]string{}
+		scanner          = bufio.NewScanner(r)
+	)
+
+	for scanner.Scan() {
+		stat := strings.Fields(scanner.Text())
+		name := "IpLocalPortRange"
+		ipLocalPortRange[name] = map[string]string{}
+		ipLocalPortRange[name]["min"] = stat[0]
+		ipLocalPortRange[name]["max"] = stat[1]
+	}
+
+	return ipLocalPortRange, scanner.Err()
+}
+
+func getSocketsStats(fileName string) (map[string]map[string]map[ephemeralPortTuple]int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+	defer file.Close()
+
+	return parseSocketsStats(file, fileName)
+}
+
+func parseSocketsStats(r io.Reader, fileName string) (map[string]map[string]map[ephemeralPortTuple]int, error) {
+	var (
+		socketsStats = map[string]map[string]map[ephemeralPortTuple]int{}
+		scanner      = bufio.NewScanner(r)
+		name         string
+	)
+
+	// Expect to have "tcp" or "udp" in fileName, return error otherwise
+	if strings.Contains(fileName, "tcp") {
+		name = "TcpSocket"
+	} else if strings.Contains(fileName, "udp") {
+		name = "UdpSocket"
+	} else {
+		return nil, fmt.Errorf("fileName (%v) should contain either \"tcp\" or \"udp\"", fileName)
+	}
+	socketsStats[name] = map[string]map[ephemeralPortTuple]int{}
+	socketsStats[name]["NbUsedPort"] = map[ephemeralPortTuple]int{}
+
+	if scanner.Scan() {
+		for scanner.Scan() {
+			stat := strings.Fields(scanner.Text())
+
+			// check file format
+			if len(stat) < 10 || len(strings.Split(stat[1], ":")) != 2 || len(strings.Split(stat[2], ":")) != 2 {
+				return nil, fmt.Errorf("file (%s) is not formatted as expected %v", fileName, len(stat))
+			}
+
+			// parse IP adresses and ports numbers
+			localAddressBytes, err := hex.DecodeString(strings.Split(stat[1], ":")[0])
+			if err != nil {
+				panic(err)
+			}
+			remoteAddressBytes, err := hex.DecodeString(strings.Split(stat[2], ":")[0])
+			if err != nil {
+				panic(err)
+			}
+			remotePortBytes, err := hex.DecodeString(strings.Split(stat[2], ":")[1])
+			if err != nil {
+				panic(err)
+			}
+			localAddress := fmt.Sprintf("%v.%v.%v.%v", localAddressBytes[0], localAddressBytes[1], localAddressBytes[2], localAddressBytes[3])
+			remoteAddress := fmt.Sprintf("%v.%v.%v.%v", remoteAddressBytes[0], remoteAddressBytes[1], remoteAddressBytes[2], remoteAddressBytes[3])
+			remotePort := fmt.Sprintf("%v%v", remotePortBytes[0], remotePortBytes[1])
+
+			tupleForEphemeralPort := ephemeralPortTuple{localIP: localAddress,
+				remoteIP:   remoteAddress,
+				remotePort: remotePort}
+
+			// increment socket count for set of local address
+			if _, exist := socketsStats[name]["NbUsedPort"][tupleForEphemeralPort]; exist {
+				socketsStats[name]["NbUsedPort"][tupleForEphemeralPort]++
+			} else {
+				socketsStats[name]["NbUsedPort"][tupleForEphemeralPort] = 1
+			}
+
+		}
+	}
+
+	return socketsStats, scanner.Err()
 }
--- collector/netstat_linux_test.go
+++ collector/netstat_linux_test.go
@@ -22,6 +22,9 @@ func TestNetStats(t *testing.T) {
 	testNetStats(t, "fixtures/proc/net/netstat")
 	testSNMPStats(t, "fixtures/proc/net/snmp")
 	testSNMP6Stats(t, "fixtures/proc/net/snmp6")
+	testIPLocalPortRange(t, "fixtures/proc/sys/net/ipv4/ip_local_port_range")
+	testTCPSocketsStats(t, "fixtures/proc/net/tcp")
+	testUDPSocketsStats(t, "fixtures/proc/net/udp")
 }
 
 func testNetStats(t *testing.T, fileName string) {
@@ -92,5 +95,66 @@ func testSNMP6Stats(t *testing.T, fileName string) {
 
 	if want, got := "8", snmp6Stats["Udp6"]["SndbufErrors"]; want != got {
 		t.Errorf("want netstat Udp6 SndbufErrors %s, got %s", want, got)
+	}
+}
+
+func testIPLocalPortRange(t *testing.T, fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	iPLocalPortRange, err := parseIPLocalPortRange(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if want, got := "32768", iPLocalPortRange["IpLocalPortRange"]["min"]; want != got {
+		t.Errorf("want netstat ip_local_port_range minErrors %s, got %s", want, got)
+	}
+
+	if want, got := "60999", iPLocalPortRange["IpLocalPortRange"]["max"]; want != got {
+		t.Errorf("want netstat IpLocalPortRange maxErrors %s, got %s", want, got)
+	}
+}
+
+func testTCPSocketsStats(t *testing.T, fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	tcpSocketsStats, err := parseSocketsStats(file, fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ephemeralPortTupleForTest := ephemeralPortTuple{localIP: "56.35.16.172",
+		remoteIP:   "215.35.16.172",
+		remotePort: "2119"}
+	if want, got := 2, tcpSocketsStats["TcpSocket"]["NbUsedPort"][ephemeralPortTupleForTest]; want != got {
+		t.Errorf("want netstat TcpSocket NbUsedPortErrors %v, got %v", want, got)
+	}
+}
+
+func testUDPSocketsStats(t *testing.T, fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	udpSocketsStats, err := parseSocketsStats(file, fileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ephemeralPortTupleForTest := ephemeralPortTuple{localIP: "0.0.0.0",
+		remoteIP:   "0.0.0.0",
+		remotePort: "00"}
+	if want, got := 1, udpSocketsStats["UdpSocket"]["NbUsedPort"][ephemeralPortTupleForTest]; want != got {
+		t.Errorf("want netstat UdpSocket NbUsedPortErrors %v, got %v", want, got)
 	}
 }
